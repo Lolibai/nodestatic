4297d528e8e2f0f315f47cb1608fd46899b7a1b7
Mykhailo Shevchenko <mikleshef@gmail.com>
2019-01-09 16:11:15 +0200

    Update Rewrite rule

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 app.use(express.static(`${__dirname}/web`));
+app.get('*', function(request, response, next) {
+  response.sendfile(__dirname + '/web/index.html');
+});
 app.listen(4201, err => {
-    if (err) {
-      console.error(err);
-    }
-    console.log('Listening on port 4201');
-  })
+  if (err) {
+    console.error(err);
+  }
+  console.log('Listening on port 4201');
+});
