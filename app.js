8f738a6c9a798b5880d4dda30b2a59340d4fc2f2
Mykhailo Shevchenko <mikleshef@gmail.com>
2019-07-24 16:50:20 +0300

    Update

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+require('dotenv').config()
+const port = process.env.PORT || 4201;
 app.use(express.static(`${__dirname}/web`));
 app.get('*', function(request, response, next) {
   response.sendfile(__dirname + '/web/index.html');
 });
-app.listen(4201, err => {
+app.listen(port, err => {
   if (err) {
     console.error(err);
   }
